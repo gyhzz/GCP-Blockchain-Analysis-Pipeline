f082bd53865ade3ecb02f0f4714d297e1743e797
Damon Goh <gohyuihao_@hotmail.com>
2024-02-25 04:11:40 +0800

    Added a dummy application to test if GKE cluster and nodes are running properly

---
+++ dummy-gke-app/dummy_app.py
@@ -0,0 +1,7 @@
+import time
+
+counter = 0
+while True:
+    counter += 1
+    print(f"Hello from the dummy application! Counter: {counter}")
+    time.sleep(5)  # Wait for 5 seconds before printing again
